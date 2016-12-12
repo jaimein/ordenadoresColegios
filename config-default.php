516451c6e3c525a7a910a9245c95e790dbcdb791
jaimein <vlc@vlc-jaime>
2016-12-12 16:31:15 +0100

    añadido desde crud inc/fun... conexion, config-def, login

---
+++ config-default.php
@@ -0,0 +1,11 @@
+<?php
+// parametros usados para conectar a la base de datos
+$host="";
+$db_name="";
+$username="";
+$password="";
+
+$intentos_login = 5;
+$tiempo_fuerzabruta = 2; //horas
+$secure = false; //por defecto no obligar a https
+$accion_default = 'lista' ;
